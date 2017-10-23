1b5ccf91a1abc8bb868ba063e52a2df353ac81bb
twirtgen <thomas.wirtgen@student.uclouvain.be>
2017-10-23 11:30:21 +0200

    dev_team 1

--- stats/models.py
+++ stats/models.py
@@ -1,14 +1,15 @@
 # encoding: utf-8
 
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 # Create your models here.
 class LoginStats(models.Model):
     user = models.ForeignKey(User)
-    user_kind = models.CharField(max_length=255, choices=(('student', 'étudiant·e'), ('professor', 'Professeur'), ('admin', 'Admin')))
+    user_kind = models.CharField(max_length=255,
+                                 choices=(('student', 'étudiant·e'), ('professor', 'Professeur'), ('admin', 'Admin')))
     when = models.DateTimeField(auto_now_add=True)
 
 
-# here create our branches
+    # here create our branches team 1
