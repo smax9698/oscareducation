b03b8be6208894457008856cd4d105bf03fb96cc
twirtgen <thomas.wirtgen@student.uclouvain.be>
2017-10-23 11:28:28 +0200

    dev_team

--- stats/models.py
+++ stats/models.py
@@ -9,3 +9,6 @@ class LoginStats(models.Model):
     user = models.ForeignKey(User)
     user_kind = models.CharField(max_length=255, choices=(('student', 'étudiant·e'), ('professor', 'Professeur'), ('admin', 'Admin')))
     when = models.DateTimeField(auto_now_add=True)
+
+
+# here create our branches
