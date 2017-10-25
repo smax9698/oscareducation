c3619ab3bf065d8d3d4f82dbb273c11466be6a28
naitali <brandon.naitali@student.uclouvain.be>
2017-10-25 14:26:19 +0200

    Database access : add field

--- stats/utils.py
+++ stats/utils.py
@@ -1,15 +1,44 @@
 from django.contrib.auth.decorators import user_passes_test
 
-import models
-
+import stats.models as models
+import datetime
 
 def user_is_superuser(function):
     return user_passes_test(lambda x: x.is_superuser)(function)
 
-
 ###################
 # Access database #
 ###################
+
+###################
+#    Update       #
+###################
+
+
+def add_exam_done_by_student(user, exam_id):
+    student = models.ExamStudent(user=user, exam=exam_id)
+    student.save()
+
+
+def add_skill_acquired_by_student(user, skill_id):
+    student = models.ResourceStudent(date_acquired=datetime.datetime, user=user, skill=skill_id)
+    student.save()
+
+
+def add_authentication_by_student(user, start_date, end_date):
+    student = models.AuthenticationStudent(user=user, date_accessed=start_date, end_of_session=end_date)
+    student.save()
+
+
+def add_resource_accessed_by_student(user, resource_id):
+    student = models.ResourceStudent(resource=resource_id, user=user, when=datetime.datetime)
+    student.save()
+
+###################
+#      Get        #
+###################
+
+
 def get_resources_accessed_by_student(student):
     accessed_by_ressources = {}
 
@@ -35,6 +64,6 @@ def get_authentication_info_by_student(student):
 
 
 def get_skill_acquired_by_student(student):
-    query = models.SkillStudent.object.get(user=student)
+    query = models.SkillStudent.objects.get(user=student)
 
     len(query)
