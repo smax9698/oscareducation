11865a6e070edd0767b7fac477cb2dc2ceb6b901
twirtgen <thomas.wirtgen@student.uclouvain.be>
2017-10-25 10:57:40 +0200

    Added some stats retrieval

--- stats/utils.py
+++ stats/utils.py
@@ -1,5 +1,37 @@
 from django.contrib.auth.decorators import user_passes_test
 
+import models
+
 
 def user_is_superuser(function):
     return user_passes_test(lambda x: x.is_superuser)(function)
+
+
+def get_resources_accessed_by_student(student):
+    accessed_by_ressources = {}
+
+    query = models.ResourceStudent.objects.get(user=student)
+
+    for item in query:
+        if item.resource in accessed_by_ressources:
+            accessed_by_ressources[item.resource] += 1
+        else:
+            accessed_by_ressources[item.resource] = 1
+
+    return accessed_by_ressources
+
+
+def get_number_of_authentication_by_student(student):
+    query = models.AuthenticationStudent.objects.get(user=student)
+
+    return len(query)
+
+
+def get_authentication_info_by_student(student):
+    return models.AuthenticationStudent.objects.get(user=student)
+
+
+def get_skill_acquired_by_student(student):
+    query = models.SkillStudent.object.get(user=student)
+
+    len(query)
