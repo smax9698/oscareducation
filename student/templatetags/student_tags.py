65e02ca20d5dbc0d101463757fc7a3076c81b33c
Gilles NGONGANG <gilles.ngongang@gmail.com>
2017-11-01 14:49:26 +0100

    Added tag for get_tab_recommended_skill

--- student/templatetags/student_tags.py
+++ student/templatetags/student_tags.py
@@ -1,5 +1,6 @@
 from django import template
 
+from student.views import get_tab_recommended_skill
 
 register = template.Library()
 
@@ -16,3 +17,8 @@ def strip_untested_skills(level_list):
 @register.filter
 def remove_closed_tests(teststudent_set):
     return teststudent_set.filter(test__running=True, test__enabled=True)
+
+@register.simple_tag(takes_context=True)
+def get_tab_recommended_skill_tag(context, of_keyword,student_skills, as_keyword, target_name):
+    context[target_name] = get_tab_recommended_skill(student_skills)
+    return ""
