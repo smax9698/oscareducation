2a709f8751c54ecb294e0e22a3b609f06d93135f
Adrien Ballet <adrien.ballet@student.uclouvain.be>
2017-11-03 11:59:36 +0100

    Tests launcher

--- skills/tests.py
+++ skills/tests.py
@@ -1,6 +1,36 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+import unittest
+
+#from __future__ import unicode_literals
 
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestSkills(unittest.TestCase):
+    '''
+    Changez le nom de classe avec un truc plus précis.
+    '''
+
+    def setUp(self):
+        '''Initialization de vos tests'''
+        pass
+
+    def tearDown(self):
+        '''De-init apres chaque test'''
+        pass
+
+
+    def test_example(self):
+        un   = 1
+        deux = 2
+
+
+        self.assertEqual(un + un, deux)
+
+    def test_example2(self):
+        un   = 1
+        deux = 2
+
+
+        self.assertEqual(un + un + un, deux)
