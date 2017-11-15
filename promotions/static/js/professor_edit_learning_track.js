8439e92bb9ecd8425a94b7ab8d60567eaeaeda84
slardinois <simon.lardinois@student.uclouvain.be>
2017-11-15 16:07:53 +0100

    added link to skill detail

--- promotions/static/js/professor_edit_learning_track.js
+++ promotions/static/js/professor_edit_learning_track.js
@@ -2,13 +2,12 @@ function learningPathController($scope, $http) {
 
     $scope.toggleSelection = function toggleSelection(learningTrack) {
         $("#alerts").html('<div class="alert alert-warning alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Toogle</div>');
-        $scope.learningTrack = ["oui", "et toi?"];
     };
 
     update_test_list = function () {
         $http.get("/professor/lesson/edit_track/" + context.lessonId + ".json").
             success(function(data, status, headers, config) {
-                $scope.learningTrack = ["salut", "ca va?"];
+                $scope.learningTrack = [];
            })
     };
 
