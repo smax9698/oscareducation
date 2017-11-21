239aeb27d800977cdf36eb9f1093ff961fd5d12d
twirtgen <thomas.wirtgen@student.uclouvain.be>
2017-11-22 00:06:02 +0100

    Dynamically change student graphs (uaa + multi user)

--- promotions/static/js/reload_graph.js
+++ promotions/static/js/reload_graph.js
@@ -1,5 +1,5 @@
-function updateCount(last_student, student, lesson){
-    $.ajax({ url: "/professor/lesson/" +lesson + "/getStat/" + student +"/" + "-1/"})
+function updateCount(last_student, student, lesson, selected_uaa){
+    $.ajax({ url: "/professor/lesson/" +lesson + "/getStat/" + student +"/" + selected_uaa.toString() + "/"})
         .success(function(data) {
             var obj = JSON.parse(data);
             $('#last_name').text("Nom : " + obj.student.last_name);
@@ -9,3 +9,47 @@ function updateCount(last_student, student, lesson){
             $('#breadcrumb').text(" Statistiques " + obj.student.first_name + " " + obj.student.last_name);
         })
 }
+
+$(document).ready(function(){
+    $('li.stud').click(function() {
+        $(this).siblings('li').removeClass('active');
+        $(this).addClass('active');
+
+        var placeholder = $('svg.graph-student').get(0).getAttribute('id');
+        var student = this.firstElementChild.getAttribute('student');
+        var lesson = this.firstElementChild.getAttribute('lesson');
+        var uaa = -1;
+
+        var uaaSelector = $('li.uaaSelector.active');
+
+        if(uaaSelector.length !== 0){
+            uaa = uaaSelector.get(0).firstElementChild.getAttribute('uaa');
+        }
+
+        updateCount(placeholder,student, lesson, uaa)
+
+    });
+
+    $('li.uaaSelector').click(function(){
+        $(this).siblings('li').removeClass('active');
+        $(this).addClass('active');
+
+
+        var currentStudentSelector = $('li.stud.active');
+
+        var placeHolder = $('svg.graph-student').get(0).getAttribute('id');
+        var student = null;
+        var lesson  = null;
+        var uaa = this.firstElementChild.getAttribute('uaa');
+
+        if (currentStudentSelector.length === 1){
+            student = currentStudentSelector.get(0).firstElementChild.getAttribute('student');
+            lesson = currentStudentSelector.get(0).firstElementChild.getAttribute('lesson');
+        }
+
+
+        updateCount(placeHolder, student, lesson, uaa);
+
+    });
+
+});
