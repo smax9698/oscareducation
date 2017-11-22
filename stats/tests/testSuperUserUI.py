f177137e3c885078f7426bee90bb28663ddcc00a
Ojonathan <40225@heb.be>
2017-11-22 13:46:44 +0100

    test LoginTest use of input and CSV button

--- stats/tests/testSuperUserUI.py
+++ stats/tests/testSuperUserUI.py
@@ -33,24 +33,9 @@ class SuperUserUI(unittest.TestCase):
         self.selenium.find_element_by_name("professor").click()
         self.selenium.find_element_by_name("admin").click()
 
-        #elem = self.selenium.find_element_by_name('html')
-        #elem.send_keys(Keys.END)
-        #time.sleep(2)
-        #verifier si l'element est visible
-        #label = self.selenium.find_element_by_xpath("//form//label[contains(text(), 'Depuis :')]")
-        #input = self.selenium.find_element_by_id("startDateLS")
-        #input.send_keys("16/11/2017")
-
-        #label.click()
-
-        #input = self.selenium.find_element_by_id('startDateLS')
-        #input.text = "11/11/2017"
-        #input.send_keys("16")
-        #input.send_keys("11")
-        #input.send_keys("2017")
-        #self.selenium.execute_script("document.querySelector('input[name='startDateLS']').value ='16/11/2017'")
-        #self.selenium.execute_script("document.getElementById('startDateLS').valueAsDate = Date.now")
-        self.selenium.execute_script("document.getElementById('startDateLS').valueAsDate = new Date(2017,11,17)")
+        #TODO Months increase by 1 and days decrease by 1
+        self.selenium.execute_script("document.getElementById('startDateLS').valueAsDate = new Date(2017, 10, 02);")
+        self.selenium.execute_script("document.getElementById('endDateLS').valueAsDate = new Date(2017, 10, 23);")
 
         time.sleep(2)
 
@@ -60,6 +45,7 @@ class SuperUserUI(unittest.TestCase):
 
     def go_to_super_user_template(self):
         #it's necessary to login as professor
+        #use a existing login and password
         self.selenium.get("http://127.0.0.1:8000/accounts/usernamelogin/")
         self.selenium.find_element_by_id("id_username").click()
         self.selenium.find_element_by_id("id_username").clear()
