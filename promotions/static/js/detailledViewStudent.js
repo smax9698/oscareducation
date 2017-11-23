d67631c48048d4b44a4e28ad29f62d67872bd86e
Ricci Dorian <dorian.ricci@outlook.com>
2017-11-23 16:13:03 +0100

    Correction affichage detailled view

--- promotions/static/js/detailledViewStudent.js
+++ promotions/static/js/detailledViewStudent.js
@@ -6,19 +6,39 @@ $(function(){
 
     // for every slide in carousel, copy the next slide's item in the slide.
     // Do the same for the next, next item.
-    $('.multi-item-carousel .item').each(function(){
-      var next = $(this).next();
-      if (!next.length) {
-        next = $(this).siblings(':first');
-      }
-      next.children(':first-child').clone().appendTo($(this));
+    if($('.multi-item-carousel .item').length == 2){
+        $('.multi-item-carousel .item').each(function(){
+            var next = $(this).next();
+            if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+            next.children(':first-child').clone().appendTo($(this));
+            // if (next.next().length>0) {
+            //   next.next().children(':first-child').clone().appendTo($(this));
+            // } else {
+            //   // $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+            // }
+        });
+    }else{
+        $('.multi-item-carousel .item').each(function(){
+            var next = $(this).next();
+            if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+            next.children(':first-child').clone().appendTo($(this));
+            if (next.next().length>0) {
+              next.next().children(':first-child').clone().appendTo($(this));
+            } else {
+              $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+            }
+        });
+    }
 
-      if (next.next().length>0) {
-        next.next().children(':first-child').clone().appendTo($(this));
-      } else {
-        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-      }
-    });
+    if($('.multi-item-carousel .item').length <= 2){
+        $(".pager .next").hide()
+        $(".pager .previous").hide()
+    }
+
     $('#theCarousel .item').first().addClass("active")
     console.log();
 })
